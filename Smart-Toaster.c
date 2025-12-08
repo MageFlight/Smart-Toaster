ba593cef300955678eb14c7f11370ad915d508db
MageFlight <112671607+MageFlight@users.noreply.github.com>
2025-12-07 21:05:11 -0500

    Get temperature sensor working

--- Smart-Toaster.c
+++ Smart-Toaster.c
@@ -3,30 +3,43 @@
 #include "hardware/spi.h"
 #include "hardware/i2c.h"
 
+#include <stdint.h>
+
 // SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
+#define SPI_PORT spi1
 #define PIN_MISO 12
 #define PIN_CS   13
 #define PIN_SCK  10
 #define PIN_MOSI 11
 
 // I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 4
 #define I2C_SCL 5
 
-#define IO_RELAY 7
+#define PIN_RELAY 7
 
-int main()
-{
+float read_temp() {
+    uint8_t buffer[1];
+
+    gpio_put(PIN_CS, 0);
+    sleep_ms(10);
+    spi_read_blocking(SPI_PORT, 0, buffer, 2);
+    sleep_ms(10);
+    gpio_put(PIN_CS, 1);
+
+    printf("Data: %d %d\n", buffer[0], buffer[1]);
+    uint16_t raw = ((uint16_t)buffer[0] << 8) | (uint16_t)buffer[1];
+    raw = raw >> 3;
+    return (float)raw * 0.25f;
+}
+
+int main() {
     stdio_init_all();
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
+    // SPI initialisation at 4MHz, 8 bits, mode 0 (CPOL=0, CPHA=0)
+    spi_init(SPI_PORT, 4000000);
+    spi_set_format(SPI_PORT, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
@@ -37,8 +50,8 @@ int main()
     gpio_put(PIN_CS, 1);
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
+    // I2C Initialisation. Using it at 100Khz.
+    i2c_init(I2C_PORT, 100000);
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
@@ -47,7 +60,7 @@ int main()
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     while (true) {
-        printf("Hello, world!\n");
+        printf("Hello, world! Temp is %f\n", read_temp());
         sleep_ms(1000);
     }
 }
