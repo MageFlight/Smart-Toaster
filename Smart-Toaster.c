b8950117495e3d9cda3aa73e403aaacc4f874fd7
MageFlight <112671607+MageFlight@users.noreply.github.com>
2025-12-07 20:15:56 -0500

    Define ports

--- Smart-Toaster.c
+++ Smart-Toaster.c
@@ -7,19 +7,19 @@
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
+#define PIN_MISO 12
+#define PIN_CS   13
+#define PIN_SCK  10
+#define PIN_MOSI 11
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_SDA 4
+#define I2C_SCL 5
 
-
+#define IO_RELAY 7
 
 int main()
 {
